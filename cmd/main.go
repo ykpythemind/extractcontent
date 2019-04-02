d5d88d81d5bd4431009ae2be094c7d65411d50cc
Yukito Ito <yukibukiyou@gmail.com>
2019-04-02 20:10:01 +0900

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -24,13 +24,13 @@ func main() {
 	size := fi.Size()
 	if size > 0 {
 		// 標準入力がある
-		os.Exit(ExtractStdin(os.Stdin, os.Stdout))
+		os.Exit(extractStdin(os.Stdin, os.Stdout))
 	} else {
-		os.Exit(ExtractRemote(os.Stdout, url))
+		os.Exit(extractRemote(os.Stdout, url))
 	}
 }
 
-func ExtractStdin(r io.Reader, w io.Writer) int {
+func extractStdin(r io.Reader, w io.Writer) int {
 	extractor := ext.NewExtractor(r, w, nil, false)
 	if err := extractor.Extract(); err != nil {
 		fmt.Fprintf(os.Stderr, "extract failed: %s\n", err)
@@ -39,7 +39,7 @@ func ExtractStdin(r io.Reader, w io.Writer) int {
 	return 0
 }
 
-func ExtractRemote(w io.Writer, url string) int {
+func extractRemote(w io.Writer, url string) int {
 	if url == "" {
 		fmt.Fprint(os.Stderr, "fail: URL is blank. \n")
 		return 1
